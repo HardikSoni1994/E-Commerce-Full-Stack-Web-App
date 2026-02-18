c33b6ef2a7d4b38b07c787e667b22542982c673d
HardikSoni1994 <soni.hardik994@gmail.com>
2026-02-19 00:42:47 +0530

    E-Commerce Full-Stack Web App

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.use("/", (req, res) => {
+  res.send("<h2>Hello, E-commerce Website is started to build ful-stack functionality real-world web-app.😎😎</h2> ");
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log("Server does not Started.!", error);
+    return false;
+  }
+  console.log("Server is Started at localhost:", PORT);
+});
